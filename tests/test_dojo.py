4e65671489624ad34e94dde47f4fcd75a5228d69
kolaempire <denokolas@gmail.com>
2017-05-15 20:58:18 +0300

    Update test_dojo.py

--- tests/test_dojo.py
+++ tests/test_dojo.py
@@ -25,25 +25,25 @@ class TestDojo(unittest.TestCase):
         and checks if the all_roomn list has a number of three items
         """
 	def test_multiple_rooms_created(self):
-    	self.assertEqual(len(self.dojo.all_rooms), 0)
-    	self.dojo.create_room('office1', 'office')
-    	self.dojo.create_room('office2', 'office')
-    	self.dojo.create_room('office3', 'office')
-    	self.assertEqual(len(self.dojo.all_rooms), 3)
+    		self.assertEqual(len(self.dojo.all_rooms), 0)
+    		self.dojo.create_room('office1', 'office')
+    		self.dojo.create_room('office2', 'office')
+    		self.dojo.create_room('office3', 'office')
+    		self.assertEqual(len(self.dojo.all_rooms), 3)
 
     	#checks that when the livingspace room list is empty it returns a list length of 0 and when room called "my_crib" is added, the list length returns 1.
-    def test_living_space_is_created(self):
-        self.assertEqual(len(self.dojo.livingspace), 0)
-        self.dojo.create_room('my_crib', 'livingspace')
-        self.assertEqual(len(self.dojo.livingspace), 1)
+    	def test_living_space_is_created(self):
+        	self.assertEqual(len(self.dojo.livingspace), 0)
+        	self.dojo.create_room('my_crib', 'livingspace')
+        	self.assertEqual(len(self.dojo.livingspace), 1)
 
-    def test_add_person_added(self):
+    	def test_add_person_added(self):
        
         #when a person is added, the all_people list length increases approprietly 
-        self.assertEqual(len(self.dojo.all_people), 0)
-        self.dojo.add_person('kobby', 'Staff')
-        self.dojo.add_person('bett', 'Fellow')
-        self.assertEqual(len(self.dojo.all_people), 2)
+        	self.assertEqual(len(self.dojo.all_people), 0)
+        	self.dojo.add_person('kobby', 'Staff')
+        	self.dojo.add_person('bett', 'Fellow')
+       	 	self.assertEqual(len(self.dojo.all_people), 2)
     
 
      
