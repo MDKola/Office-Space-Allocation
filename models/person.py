6b59a992410e28328c432d8e13620928d703d6d3
kolaempire <denokolas@gmail.com>
2017-05-16 17:07:51 +0300

    Implemented the models functionality

--- models/person.py
+++ models/person.py
@@ -1,11 +1,25 @@
 class Person(object):
-	def __init__(self, name):
-		pass
+	def __init__(self, name, needs_accomodation):
+		self.name = name
+		self.needs_accomodation = needs_accomodation
+		self.office = None
 
+	def __repr__(self):
+		return self.name
+
+#creates a Fellow class which inherits from Person
 class Fellow(Person):
-	def __init__(self):
-		pass
+	def __init__(self, name, needs_accomodation):
+		super(Fellow,self).__init__(name)
+
+	def __repr__(self):
+		return self.name
+
+
 
 class Staff(Person):
 	def __init__(self):
-		pass
+		super(Staff, self).__init__(name)
+
+	def __repr__(self):
+		return self.name
--- models/room.py
+++ models/room.py
@@ -4,12 +4,28 @@ class Room(object):
 		self.room_type = room_type
 		self.max_occupants = []
 
+	def __repr__(self):
+		return self.name
+
+	def add_person(self,person):
+		self.capacity = self.capacity -1
+		return self.capacity
+
+	def room_type(self):
+		return room.__class__.__name__
 
 
 class Office(Room):
-	def __init__(self, name,max_occupants=6):
+	def __init__(self, name, room_type,max_occupants=6):
+		super(Office, self).__init__(name, room_type,max_occupants=6)
+
+	def __repr__(self):
+		return self.name
 
 
 class Living_space(Room):
-	def __init__(self,max_occupants=4):
-		
+	def __init__(self,name,room_type,max_occupants=4):
+		super(Living_space, self).__init__(name, room_type,max_occupants=4)
+
+	def __repr__(self):
+		return self.name
