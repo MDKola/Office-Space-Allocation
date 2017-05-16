7eea7b8e206e1d2a427b33b7d8c2c0ef83d1f6bb
kolaempire <denokolas@gmail.com>
2017-05-16 10:57:56 +0300

    modified room module

--- models/room.py
+++ models/room.py
@@ -2,12 +2,12 @@ class Room(object):
 	def __init__(self, name, room_type, max_occupants):
 		self.name = name
 		self.room_type = room_type
-		self.max_occupants = max_occupants
+		self.max_occupants = []
 
 
 
 class Office(Room):
-	def __init__(self, max_occupants=6):
+	def __init__(self, name,max_occupants=6):
 
 
 class Living_space(Room):
